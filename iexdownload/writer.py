59c14352c9af64bedb7596f73f440ea94ad7215e
Shawn Lin <xiaoliang.lin@columbia.edu>
2019-12-19 01:17:40 -0500

    Switch to postgres, implement upsert

--- iexdownload/writer.py
+++ iexdownload/writer.py
@@ -7,40 +7,44 @@ from sqlalchemy import create_engine
 
 class DBWriter(object):
 
-    def __init__(self, user, password, db, host="localhost", sql_dialect="mysql", port="3306"):
+    def __init__(self, user, password, db, host="localhost", sql_dialect="postgres", port="5432"):
 
-        db_connectors = {"mysql": "mysql+pymysql"}
-        self.con = create_engine(f"{db_connectors[sql_dialect]}://{user}:{password}@{host}:{port}/{db}")
+        db_connectors = {"postgres": "postgres"}
+        self.engine = create_engine(f"{db_connectors[sql_dialect]}://{user}:{password}@{host}:{port}")
+        self.create_db_if_not_exist(db, self.engine)
+        self.engine = create_engine(f"{db_connectors[sql_dialect]}://{user}:{password}@{host}:{port}/{db}")
+
+    @staticmethod
+    def create_db_if_not_exist(db, engine):
+
+        db_list = pd.read_sql("SELECT datname FROM pg_database", engine)["datname"].values
+
+        if db not in db_list:
+            con = engine.connect()
+            con.connection.connection.set_isolation_level(0)
+            con.execute(f'create database {db}')
+            con.connection.connection.set_isolation_level(1)
 
     def write_df(self, df, table: str, pk: List[str] = []):
 
         try:
-            df.to_sql(name=table, con=self.con, if_exists="fail", index=False)
+            df.to_sql(name=table, con=self.engine, if_exists="fail", index=False)
+            set_pk = f"""
+            ALTER TABLE {table}
+              ADD PRIMARY KEY ({", ".join(pk)})
+            """
+            self.engine.execute(set_pk)
 
         except ValueError:
 
-            query_cols = f"""
-            SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS 
-            WHERE TABLE_NAME = '{table}'
-            """
-            cols = pd.read_sql(query_cols, con=self.con)["COLUMN_NAME"].values
-            pk = list(set(pk) & set(cols))
-            cols = list(set(cols) - set(pk))
             temp_table = f"temp_{table}_{random.randint(0, 1e20)}"
-            df.to_sql(name=temp_table, con=self.con, if_exists="fail")
+            df.to_sql(name=temp_table, con=self.engine, if_exists="fail", index=False)
 
-            sql_update = f"""
-            UPDATE {table} AS main, {temp_table} AS temp
-            SET {", ".join([f"main.{col} = temp.{col}" for col in cols])}
-            WHERE {" AND ".join([f"main.{key} = temp.{key}" for key in pk])}
+            sql_upsert = f"""
+            INSERT INTO {table} 
+            SELECT * FROM {temp_table}
+            ON CONFLICT ({", ".join(pk)}) DO UPDATE
+              SET {", ".join(f'"{col}" = excluded."{col}"' for col in set(df.columns) - set(pk))}
             """
-            print(sql_update)
-            self.con.execute(sql_update)
-            self.con.execute(f"DROP TABLE {temp_table}")
-
-
-test = DBWriter(user="root", password="password", db="DW1")
-tempdf = pd.read_sql("SELECT * FROM nav", con=test.con).head()
-test.write_df(pd.read_sql("SELECT * FROM nav", con=test.con).head(), "test", pk=["tradeday", "account"])
-tempdf["nav"][1] = 1000
-test.write_df(tempdf, "test", pk=["tradeday", "account"])
+            self.engine.execute(sql_upsert)
+            self.engine.execute(f"DROP TABLE {temp_table}")
