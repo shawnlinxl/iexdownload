b60c890fa4b954eaa304042b8ebdb2ec335c0d36
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-07-16 18:55:33 +0000

    update download process

--- iexdownload/downloader.py
+++ iexdownload/downloader.py
@@ -1,25 +1,17 @@
 from typing import List
-
-from iexcloud.reference import Reference
-from iexcloud.stock import Stock
-
-from iexdownload.writer import DBWriter
+from iexcloud import Stock
 
 
 class Downloader(object):
-    def __init__(self, symbol: List[str] = None, db=List[DBWriter]):
+    def __init__(self):
 
-        self.ref = Reference()
-        self.db = db
+        self.symbol = set()
+        self.stock = dict()
+        self.writer = list()
 
-        if symbol is None:
-            self.symbol = set()
-            self.stock = dict()
-        else:
-            if type(symbol) is not list:
-                symbol = [symbol]
-            self.symbol = set(symbol)
-            self.stock = {key: Stock(key, output="pandas") for key in self.symbol}
+    def add_writer(self, writer):
+
+        self.writer.append(writer)
 
     def add_symbol(self, symbol: List[str]):
 
@@ -31,22 +23,31 @@ class Downloader(object):
             if s not in self.stock:
                 self.stock[s] = Stock(s, output="pandas")
 
-    def download_price(self, time_range="5d") -> None:
+    def download_price(self, time_range: str = "5d") -> None:
 
         for stock in self.stock.values():
 
-            price = stock.get_price(time_range=time_range)
+            data = stock.get_price(time_range=time_range)
 
-            if price.shape[0] > 0:
-                for writer in self.db:
-                    writer.write_df(price, table="price", pk=["date", "symbol"])
+            if data.shape[0] > 0:
+                for writer in self.writer:
+                    writer.write(data, f"price/{stock.symbol}.csv")
 
-    def download_dividend(self, time_range="1m") -> None:
+    def download_dividend(self, time_range: str = "1m") -> None:
 
         for stock in self.stock.values():
 
-            dividend = stock.get_dividend(time_range=time_range)
+            data = stock.get_dividend(time_range=time_range)
 
-            if dividend.shape[0] > 0:
-                for writer in self.db:
-                    writer.write_df(dividend, table="dividend", pk=["exDate", "symbol"])
+            if data.shape[0] > 0:
+                for writer in self.writer:
+                    writer.write(data, f"dividend/{stock.symbol}.csv")
+
+    def download_split(self, time_range: str = "1m") -> None:
+
+        for stock in self.stock.values():
+            data = stock.get_split(time_range=time_range)
+
+            if data.shape[0] > 0:
+                for writer in self.writer:
+                    writer.write(data, f"split/{stock.symbol}.csv")
--- iexdownload/writer.py
+++ iexdownload/writer.py
@@ -1,57 +1,25 @@
-import random
-from typing import List
-
+import os
 import pandas as pd
-from sqlalchemy import create_engine
 
 
-class DBWriter(object):
-    def __init__(
-        self, user, password, db, host="localhost", sql_dialect="postgres", port="5432"
-    ):
+class CsvWriter:
+    def __init__(self, path: str = None):
 
-        db_connectors = {"postgres": "postgres"}
-        self.engine = create_engine(
-            f"{db_connectors[sql_dialect]}://{user}:{password}@{host}:{port}"
-        )
-        self.create_db_if_not_exist(db, self.engine)
-        self.engine = create_engine(
-            f"{db_connectors[sql_dialect]}://{user}:{password}@{host}:{port}/{db}"
-        )
+        if path is not None:
+            self.path = path
+        else:
+            self.path = os.getcwd()
 
     @staticmethod
-    def create_db_if_not_exist(db, engine):
+    def create_dir_if_not_exist(path: str):
 
-        db_list = pd.read_sql("SELECT datname FROM pg_database", engine)[
-            "datname"
-        ].values
+        dir_path = os.path.dirname(path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
 
-        if db not in db_list:
-            con = engine.connect()
-            con.connection.connection.set_isolation_level(0)
-            con.execute(f"create database {db}")
-            con.connection.connection.set_isolation_level(1)
+    def write(self, df: pd.DataFrame, path: str):
 
-    def write_df(self, df, table: str, pk: List[str] = []):
+        full_path = os.path.join(self.path, path)
+        self.create_dir_if_not_exist(full_path)
 
-        try:
-            df.to_sql(name=table, con=self.engine, if_exists="fail", index=False)
-            set_pk = f"""
-            ALTER TABLE {table}
-              ADD PRIMARY KEY ({", ".join(f'"{key}"' for key in pk)})
-            """
-            self.engine.execute(set_pk)
-
-        except ValueError:
-
-            temp_table = f"temp_{table}_{random.randint(0, 1e20)}"
-            df.to_sql(name=temp_table, con=self.engine, if_exists="fail", index=False)
-
-            sql_upsert = f"""
-            INSERT INTO {table} 
-            SELECT * FROM {temp_table}
-            ON CONFLICT ({", ".join(f'"{key}"' for key in pk)}) DO UPDATE
-              SET {", ".join(f'"{col}" = excluded."{col}"' for col in set(df.columns) - set(pk))}
-            """
-            self.engine.execute(sql_upsert)
-            self.engine.execute(f"DROP TABLE {temp_table}")
+        df.to_csv(full_path, index=False)
