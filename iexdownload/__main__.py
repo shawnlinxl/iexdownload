5149c1fa22de4a7f0fc853f7f90399a08db5ffdd
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-07-16 19:44:39 +0000

    add datasets

--- iexdownload/__main__.py
+++ iexdownload/__main__.py
@@ -1,44 +1,61 @@
 from iexdownload.downloader import Downloader
-from iexdownload.writer import DBWriter
+from iexdownload.writer import CsvWriter
+from iexdownload.etf import (
+    commodity,
+    currency,
+    us_sector,
+    global_sector,
+    us_equity,
+    global_equity,
+    bond,
+)
+from iexcloud import set_mode, set_token
 
-writers = [
-    DBWriter(user="postgres", password="password", db="iex", port="5432"),  # Production
-    DBWriter(user="postgres", password="password", db="iex", port="5431"),  # Staging
+set_mode("PRODUCTION")
+set_token("TOKEN")
+
+production_writer = CsvWriter("./production")
+staging_writer = CsvWriter("./staging")
+
+download = Downloader()
+
+holdings = [
+    "KR",
+    "JD",
+    "KO",
+    "TWTR",
+    "INTU",
+    "EBAY",
+    "CMG",
+    "LQD",
+    "YUMC",
+    "SQ",
+    "GPRO",
+    "PEP",
+    "WMT",
+    "MCD",
+    "TEAM",
+    "APRN",
+    "CRM",
+    "SPOT",
+    "STZ",
+    "GOOGL",
+    "FB",
+    "ONDK",
+    "T",
+    "KWEB",
+    "KHC",
+    "TPR",
+    "PFE",
+    "DIS",
+    "TCEHY",
 ]
 
-download = Downloader(
-    symbol=[
-        "KR",
-        "JD",
-        "KO",
-        "TWTR",
-        "INTU",
-        "EBAY",
-        "CMG",
-        "LQD",
-        "YUMC",
-        "SQ",
-        "GPRO",
-        "PEP",
-        "WMT",
-        "MCD",
-        "TEAM",
-        "APRN",
-        "CRM",
-        "SPOT",
-        "STZ",
-        "GOOGL",
-        "FB",
-        "ONDK",
-        "T",
-        "KWEB",
-        "KHC",
-        "TPR",
-        "PFE",
-        "DIS",
-        "TCEHY",
-    ],
-    db=writers,
-)
-download.download_price(time_range="1m")
-download.download_dividend(time_range="5y")
+
+download.add_symbol(commodity)
+download.add_writer(production_writer)
+download.add_writer(staging_writer)
+
+download.download_price()
+download.download_dividend()
+download.download_split()
---
+++ iexdownload/etf.py
@@ -0,0 +1,131 @@
+commodity = [
+    # "GLD",  # Gold
+    "SLV",  # Silver/slv
+    "PPLT",  # Platinum
+    "DBB",  # Industrial Metals
+    "DBA",  # Agricultural Commodities
+    "DBO",  # Oil
+    "UNG",  # Natural Gas
+]
+
+currency = [
+    "FXE",  # Euro
+    "FXB",  # British Pound
+    "FXY",  # Japanese Yen
+    "UUP",  # US Dollar
+    "CYB",  # Chinese Yuan
+]
+
+us_sector = [
+    "XLK",  # Technology
+    "XLV",  # Healthcare
+    "XLY",  # Consumer Discretionary
+    "XLP",  # Consumer Staples
+    "XLU",  # Utilities
+    "XLB",  # Basic Materials
+    "XLI",  # Industrial
+    "XLF",  # Financial Services
+    "XLE",  # Energy
+]
+
+global_sector = [
+    "IXN",  # Technology
+    "IXJ",  # Healthcare
+    "RXI",  # Consumer Discretionary
+    "KXI",  # Consumer Staples
+    "IXP",  # Telecommunications
+    "JXI",  # Utilities
+    "MXI",  # Basic Materials
+    "EXI",  # Industrial
+    "IXG",  # Financial Services
+    "IXC",  # Energy
+]
+
+us_equity = [
+    "QQQ",  # NASDAQ 100
+    "SPY",  # S&P 500
+    "DIA",  # DJIA
+    "MDY",  # Mid Cap
+    "IJR",  # Small Cap
+]
+
+global_equity = [
+    "ADRE",  # Emerging Markets
+    "BKF",  # BRICs
+    "ACWI",  # World Equities
+    "SPDW",  # World ex-US
+]
+
+bond = [
+    "TLT",  # 20+ Year Treasury Bonds
+    "TIP",  # TIPS - US
+    "BND",  # Aggregate Bonds - US
+    "BWX",  # International Govt. Bonds
+    "PHB",  # High Yield Bonds - US
+]
+
+symbol = [
+    "IVV",
+    "VOO",
+    "VTI",
+    "AGG",
+    "VEA",
+    "IEFA",
+    "VWO",
+    "VUG",
+    "IWF",
+    "LQD",
+    "IEMG",
+    "VTV",
+    "EFA",
+    "VIG",
+    "IJH",
+    "IWM",
+    "VCIT",
+    "IWD",
+    "USMV",
+    "VGT",
+    "VO",
+    "VCSH",
+    "IVW",
+    "BNDX",
+    "VNQ",
+    "HYG",
+    "VB",
+    "IAU",
+    "VYM",
+    "ITOT",
+    "BSV",
+    "VEU",
+    "EEM",
+    "IEF",
+    "IWB",
+    "SHY",
+    "VXUS",
+    "SHV",
+    "MBB",
+    "SCHX",
+    "IWR",
+    "IXUS",
+    "SCHF",
+    "QUAL",
+    "IGSB",
+    "VV",
+    "MUB",
+    "GDX",
+    "IVE",
+    "PFF",
+    "SDY",
+    "SCHB",
+    "BIL",
+    "EMB",
+    "GOVT",
+    "MINT",
+    "VT",
+    "BIV",
+    "JPST",
+    "DVY",
+    "VBR",
+    "IWP",
+    "RSP",
+]
